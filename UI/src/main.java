6ea17c7f7202dac9d3119a9ef07fa0aff52501bd
VTzy <Truong.nv215495@sis.hust.edu.vn>
2024-01-02 17:16:04 +0700

    2/1/24

--- UI/src/main.java
+++ UI/src/main.java
@@ -1,39 +1,29 @@
-import javafx.scene.layout.*;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import org.json.*;
 
 import javafx.application.Application;
-import javafx.scene.*;
 import javafx.stage.Stage;
-import javafx.scene.control.Button;
-import java.util.Map;
+
+import javax.swing.*;
+import java.io.IOException;
 
 public class main extends Application {
 
-    private static Stage appStage;
-    private static Scene currentScene;
-//    private static Map<Scene, Scene> prevSceneMap;
-    private static int sceneHeight;
-    private static int sceneWidth;
-
     @Override
-    public void start(Stage appStage) throws Exception {
-        sceneHeight = 900;
-        sceneWidth = 1600;
-        main.appStage = appStage;
-        Button bti1 = new Button("Xin dấu");
-        Button bti2 = new Button("Xường Trinh");
-        GridPane gP = new GridPane();
-        gP.setVgap(50);
-        gP.setHgap(120);
-//        gP.setLayoutX(130);
-//        gP.setLayoutY(280);
-        gP.add(bti1, 0, 0);
-        gP.add(bti2, 1, 0);
-        currentScene = new Scene(gP, sceneWidth,sceneHeight);
-//        prevSceneMap = new HashMap<Scene, Scene>();
-        appStage.setTitle("Café App");
-        appStage.setScene(currentScene);
-        appStage.show();
+    public void start(Stage stage) {
+        try {
+
+            Parent root = FXMLLoader.load(getClass().getResource("IngredientScene.fxml"));
+            Scene scene = new Scene(root);
+            stage.setScene(scene);
+            stage.show();
+
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
     }
     public static void main(String[] args) {
         readInfomation st = new readInfomation();
---
+++ UI/src/manageIngredient.java
@@ -0,0 +1,30 @@
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class manageIngredient {
+//    private static Scene currentScene;
+    //    private static Map<Scene, Scene> prevSceneMap;
+    private static int sceneHeight;
+    private static int sceneWidth;
+//    static class mIngre {
+        public static void buildScene() {
+            //        sceneHeight = 900;
+            //        sceneWidth = 1600;
+            //        Button bti1 = new Button("Xin dấu");
+            //        Button bti2 = new Button("Xường Trinh");
+            //        GridPane gP = new GridPane();
+            //            gP.setVgap(50);
+            //            gP.setHgap(120);
+            //    //        gP.setLayoutX(130);
+            //    //        gP.setLayoutY(280);
+            //            gP.add(bti1, 0, 0);
+            //            gP.add(bti2, 1, 0);
+//            manageIngredient mI = new manageIngredient();
+//            Parent root = FXMLLoader.load(getClass().getResource("IngredientScene.fxml"));
+//            return new Scene(root);
+        }
+//    }
+}
